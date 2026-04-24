217761b78df64273253609cf04e22cb8e3199af5
chanmakara <sokchanmakara04@gmail.com>
2026-04-25 02:15:00 +0700

    disable sourcemap warning

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  sourcemap: {
+    server: false,
+    client: false
+  },
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
     layoutTransition: { name: 'layout', mode: 'out-in' },
